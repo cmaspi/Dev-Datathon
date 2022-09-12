162172d90fd49eb2653e80bbf6607fbf025be487
cmaspi <chiragmehta1010@gmail.com>
2022-09-12 22:11:10 +0530

    beautifief the code

--- app/nlp.py
+++ app/nlp.py
@@ -1,10 +1,11 @@
 from transformers import pipeline
 
+
 class NLP:
     def __init__(self) -> None:
         self.model = pipeline("summarization", model="t5-small",
                               tokenizer="t5-small", framework="tf")
 
-    def summarize(self, reviews:str, max_length=100, min_length=5) -> str:
+    def summarize(self, reviews: str, max_length=100, min_length=5) -> str:
         res = self.model(reviews, max_length=max_length, min_length=min_length)
         return res
