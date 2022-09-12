8d0603365874623653858443cf4782a41defbe37
Dishank422 <dishankjain0112358@gmail.com>
2022-09-12 08:49:21 +0530

    started integrating Auth0

--- course_feedback/user/models.py
+++ course_feedback/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class student(User):
+    roll_no = models.CharField(max_length=20, primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
