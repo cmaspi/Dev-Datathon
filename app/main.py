e3cf3e104a59b14ccb90d9523acb78fade13fecc
cmaspi <chiragmehta1010@gmail.com>
2022-09-12 22:00:34 +0530

    added main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,33 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-# from app.nlp import NLP
+from app.nlp import NLP
+
+
+class Message(BaseModel):
+    input: str
+    output: str | None = None
+
+
+app = FastAPI()
+nlp = NLP()
+
+origins = [
+    "https://localhost",
+    "http://localhost:3000",
+    "https:127:0.0.1:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['POST'],
+    allow_heads=["*"],
+)
+
+
+@app.post("/summarize/")
+async def summarize(message: Message):
+    message.output = nlp.generate(prompt=message.input)
+    return message.output
