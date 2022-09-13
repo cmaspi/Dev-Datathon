e5bf55dc8dcfc760b9abeb87bee53bd66c960727
cmaspi <chiragmehta1010@gmail.com>
2022-09-13 14:29:45 +0530

    Removed Dockerfile, added error handling, response
    1. got rid of dockerfile since it wasn't be necessary from now own. The
    project is shifting to fastAPI.
    2. Added error handling to main.py
    3. Added a connector file to get response from given url.

---
+++ app/sample_connector.py
@@ -0,0 +1,42 @@
+import requests
+
+"""
+curl -X 'POST' \
+                    'http://127.0.0.1:8080/summarize/' \
+                    -H 'accept: application/json' \
+                    -H 'Content-Type: application/json' \
+                    -d '{
+                "input": "<enter the feedback here>",
+              }'
+"""
+
+json_data = {
+        'input': "fcuk",
+            }
+
+
+def get_response(url: str, text: str) -> dict:
+    """
+    Returns whether the retrieval was successful, and the summary
+
+    Args:
+        url (str): the url to the service
+        input (str): the text to be summarized
+
+    Returns:
+        dict: contains keys success, summary
+    """
+    headers = {
+            "accept": "application/json",
+              }
+    json_data = {
+                    'input': text
+                }
+    response = requests.post(url, json=json_data, headers=headers).json()
+    return response
+
+
+if __name__ == '__main__':
+    text = 'This is a piece of text that should be summarized by this app'
+    url = 'http://0.0.0.0:1234/summarize/'    
+    print(get_response(url, text))
