ea660d036fd98ddf9603677521a805bb6f1f8c79
Dishank422 <dishankjain0112358@gmail.com>
2022-09-13 11:31:37 +0530

    Authentication done

--- course_feedback/user/views.py
+++ course_feedback/user/views.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view
 from .models import student
 from django.contrib.auth import login
 from rest_framework.response import Response
+from google.oauth2 import id_token
+from google.auth.transport import requests
 
 
 # Create your views here.
@@ -11,7 +13,8 @@ def populate(file):
 
 
 def email_from_token(token):
-    return token
+    idinfo = id_token.verify_oauth2_token(token, requests.Request(), "CLIENT_ID")
+    return idinfo['email']
 
 
 def check_registration(token):
@@ -21,7 +24,7 @@ def check_registration(token):
 
 @api_view(['POST'])
 def signup(request):
-    email = request.POST.get('email')
+    email = email_from_token(request.POST.get('token'))
     username = request.POST.get('username')
     grade_card = request.POST.get('grade_card')
     user = student(email=email, username=username, grade_card=grade_card)
