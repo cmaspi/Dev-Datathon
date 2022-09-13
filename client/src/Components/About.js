898bae0ae79f550538065e206c6dabff1625d6d3
Ananthoju-Pranav-Sai <ai20btech11004@iith.ac.in>
2022-09-14 00:02:27 +0530

    added about page

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -1,8 +1,92 @@
 import React from 'react'
+import "./About.css"
 
 const About = () => {
   return (
-    <div><h1>About</h1></div>
+    <div>
+      <div class='about-section'>
+        <h1>About Us</h1>
+        <p>Text about ourselves. Lorem ipsum ipsum da lorem wa</p>
+        <p>Just some blah blah text. Needs to be changed anyways</p>
+      </div>
+      <center>
+        <h2>Our Team</h2>
+      </center>
+      <div class="row">
+        <div class="column">
+          <div class="card">
+            <div class="container">
+              <h2>Chirag Mehta</h2>
+              <p class="title">AI20BTECH11006</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>ai20btech11006@iith.ac.in</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <div class="container">
+              <h2>Dishank Jain</h2>
+              <p class="title">AI20BTECH11011</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>ai20btech11011@iith.ac.in</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <div class="container">
+              <h2>Pranav Sai</h2>
+              <p class="title">AI20BTECH11004</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>ai20btech11004@iith.ac.in</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <div class="container">
+              <h2>Umesh Kalvakuntla</h2>
+              <p class="title">CS20BTECH11024</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>cs20btech11024@iith.ac.in</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <div class="container">
+              <h2>Yashas Tadikamalla</h2>
+              <p class="title">AI20BTECH11027</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>ai20btech11027@iith.ac.in</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="card">
+            <div class="container">
+              <h2>Vaibhav Ramola</h2>
+              <p class="title">EP20BTECH110025</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>ep20btech11025@iith.ac.in</p>
+              <p><button class="button">Contact</button></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
