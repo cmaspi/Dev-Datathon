55d1b37aed298cbddcd9a38a12a670c8e9a9c09a
YashasTadikamalla <yashast5678@gmail.com>
2022-09-13 18:20:55 +0530

    upvote_count

--- course_feedback/reviews/serializers.py
+++ course_feedback/reviews/serializers.py
@@ -4,6 +4,11 @@ from .models import course_review
 
 class Review_serializer(serializers.ModelSerializer):
 
+    upvoters_count=serializers.SerializerMethodField()
+
     class Meta:
         model = course_review
-        fields = '__all__'
+        fields = ['student','course','review','upvoters_count']
+
+    def get_upvoters_count(self,obj):
+        return obj.upvoters.count()
