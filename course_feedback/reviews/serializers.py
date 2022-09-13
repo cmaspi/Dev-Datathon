085dc5d4ef27051d11aa90929135be46bc5b9029
Dishank422 <dishankjain0112358@gmail.com>
2022-09-13 19:55:28 +0530

    database populating now

--- course_feedback/reviews/serializers.py
+++ course_feedback/reviews/serializers.py
@@ -3,12 +3,11 @@ from .models import course_review
 
 
 class Review_serializer(serializers.ModelSerializer):
-
-    upvoters_count=serializers.SerializerMethodField()
+    upvoters_count = serializers.SerializerMethodField()
 
     class Meta:
         model = course_review
-        fields = ['student','course','review','upvoters_count']
+        fields = ['student', 'course', 'review', 'upvoters_count']
 
-    def get_upvoters_count(self,obj):
+    def get_upvoters_count(self, obj):
         return obj.upvoters.count()
--- course_feedback/user/views.py
+++ course_feedback/user/views.py
@@ -7,16 +7,15 @@ from google.auth.transport import requests
 
 import sys
 from bs4 import BeautifulSoup
+
 sys.path.append("../")
 from course.models import course as c
 from grades.models import grade as g
 
+
 # Create your views here.
 def populate(file, st):
-    
-    with open(f'{file}') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
-    
+    soup = BeautifulSoup(file.open(), 'html.parser')
     Courses = soup.find_all('ul', {'class': 'subCnt'})
     Duration = None
     # f = open('sample.txt', 'w')  # to be deleted
@@ -26,10 +25,10 @@ def populate(file, st):
             if check is not None:
                 Duration = check.text
             else:
-                CourseCode = course.find('span', {'class': 'col1'}).text
-                CourseName = course.find('span', {'class': 'col2'}).text
-                instructor = course.find('span', {'class': 'col7'}).text
-                grade = course.find('span', {'class': 'col8'}).text
+                CourseCode = course.find('span', {'class': 'col1'}).text.strip()
+                CourseName = course.find('span', {'class': 'col2'}).text.strip()
+                instructor = course.find('span', {'class': 'col7'}).text.strip()
+                grade = course.find('span', {'class': 'col8'}).text.strip()
                 # temporary
                 '''
                 f.write(f'Course Code: {CourseCode}, Course Name: {CourseName}, ' +
@@ -37,18 +36,26 @@ def populate(file, st):
                 '''
 
                 # for course population
-                course_=c(name=CourseName,prof=instructor, code=CourseCode, offering=Duration)
+                course_ = c(name=CourseName, prof=instructor, code=CourseCode, offering=Duration)
                 course_.save()
 
                 # for grades population
-                grade_=g(student=st, course=course_,grade=grade)
+                print("Bababa", grade, CourseName)
+                dic = {"A+": 10, "A": 10, "A-": 9, "B": 8, "B-": 7, "C": 6, "C-": 5, "D": 4, "FR": 0}
+                if grade in dic:
+                    grade = dic[grade]
+                else:
+                    grade = None
+                print("Hahahahaha", grade)
+                grade_ = g(student=st, course=course_, grade=grade)
                 grade_.save()
 
     # f.close()
 
 
 def email_from_token(token):
-    idinfo = id_token.verify_oauth2_token(token, requests.Request(), "739559908237-bf47v9n6rocl61d6r0hktqm2jh3564t9.apps.googleusercontent.com")
+    idinfo = id_token.verify_oauth2_token(token, requests.Request(),
+                                          "739559908237-bf47v9n6rocl61d6r0hktqm2jh3564t9.apps.googleusercontent.com")
     return idinfo['email']
 
 
@@ -59,10 +66,11 @@ def check_registration(token):
 
 @api_view(['POST'])
 def signup(request):
-    email = email_from_token(request.POST.get('token'))
+    # email = email_from_token(request.POST.get('token'))
+    email = request.POST.get('email')
     username = request.POST.get('username')
-    grade_card = request.POST.get('grade_card')
+    grade_card = request.FILES['grade_card']
     user = student(email=email, username=username, grade_card=grade_card)
     user.save()
-    populate(grade_card,user)
+    populate(grade_card, user)
     return Response({0})
