5d98b1b1c624b2f4e4ac481e1248bcd44b8421f2
Umesh Kalvakuntla <umeshkalvakuntla26@gmail.com>
2022-09-14 23:33:47 +0530

    Add form as submiy

--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -15,17 +15,20 @@ const SignUp = () => {
       method: "POST",
       headers: {
         Authorization: token,
-        "Content-Type": "",
+        "Content-Type": "application/json",
       },
     })
-      .then((res) => console.log(res))
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
       .catch((err) => console.log(err));
   };
 
   return (
     <>
       <h3>Sign Up</h3>
-      <Form>
+      <Form onSubmit={onSubmit()}>
         <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
           <Form.Label column sm="2">
             Email
@@ -44,7 +47,7 @@ const SignUp = () => {
           <Form.Control type="file" size="lg" />
         </Form.Group>
 
-        <Button variant="primary" onClick={() => onSubmit()}>
+        <Button variant="primary" type="submit">
           Submit
         </Button>
       </Form>
