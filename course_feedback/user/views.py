50e77595f82390f1311e5663a8c680ece5dba1de
Dishank422 <dishankjain0112358@gmail.com>
2022-09-14 23:32:17 +0530

    signup

--- course_feedback/user/views.py
+++ course_feedback/user/views.py
@@ -69,9 +69,9 @@ def signup(request):
     try:
         email = email_from_token(request.headers.get('Authorization'))
         # email = request.POST.get('email')
-        request_body = json.loads(request.body.decode('utf-8'))
-        grade_card = request_body['grade_card']
-        # grade_card = request.FILES['grade_card']
+        # request_body = json.loads(request.body.decode('utf-8'))
+        # grade_card = request_body['grade_card']
+        grade_card = request.FILES['grade_card']
         user = student(email=email, grade_card=grade_card)
         user.save()
         populate(grade_card, user)
